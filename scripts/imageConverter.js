db22d2e5e2a60e457b41b2599ea2965a6768f084
Wouter <woutervandervelde2000@hotmail.com>
2022-04-05 23:17:08 +0200

    updated filesystem, added gitignore

---
+++ scripts/imageConverter.js
@@ -0,0 +1,6 @@
+/** TODO
+ * Approach 3, make user use local image and convert it to base64
+ * https://stackoverflow.com/questions/6150289/how-can-i-convert-an-image-into-base64-string-using-javascript
+ * 
+ * Use this image to save in LocalStorage so user can pick image from history 
+**/
