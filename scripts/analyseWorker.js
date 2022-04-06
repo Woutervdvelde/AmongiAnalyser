220f2bd58400e07983c76cdd1f1da2214619d604
Wouter <woutervandervelde2000@hotmail.com>
2022-04-07 00:58:34 +0200

    initial code for worker
    nasty workaround for local worker file

---
+++ scripts/analyseWorker.js
@@ -0,0 +1,176 @@
+const worker_function = () => {
+    onmessage = (message) => {
+        checkImageForAmongy(message.data[0]);
+        canvasWidth = message.data[1];
+        canvasHeight = message.data[2];
+    }
+
+    class Amongy {
+        static Type = {
+            SHORT: "short",
+            TRADITIONAL: "traditional",
+            SITTING: "sitting",
+            NONE: "none"
+        }
+
+        type = Amongy.Type.NONE;
+        pixels = [];
+
+        constructor(type, pixels) {
+            this.type = type;
+            this.pixels = pixels;
+        }
+    }
+
+    class Pixel {
+        x = 0;
+        y = 0;
+        color = "" //RGB
+
+        constructor(x, y, color) {
+            this.x = x;
+            this.y = y;
+            this.color = color;
+        }
+    }
+
+    const flip = (variant) => {
+        let cords = [];
+        oldX = Math.max(...variant.cords.filter(v => v.y == 0).map(v => v.x));
+        variant.cords.forEach(cord => {
+            cords.push({ x: Math.abs(cord.x - oldX), y: cord.y, c: cord.c });
+        });
+
+        return new Variant(variant.type, true, cords);
+    }
+
+    class Variant {
+        type = Amongy.Type.NONE;
+        flipped = false;
+        cords = [];
+
+        constructor(type, flipped, cords) {
+            this.type = type;
+            this.flipped = flipped;
+            this.cords = cords;
+        }
+    }
+
+    const traditionalboy = new Variant(
+        Amongy.Type.TRADITIONAL,
+        false,
+        [
+            { x: 0, y: 0, c: [0] },
+            { x: 1, y: 0, c: [0] },
+            { x: 2, y: 0, c: [0] },
+            { x: -1, y: 1, c: [0] },
+            { x: 0, y: 1, c: [0] },
+            { x: 1, y: 1, c: [3] },
+            { x: 2, y: 1, c: [3] },
+            { x: -1, y: 2, c: [0] },
+            { x: 0, y: 2, c: [0] },
+            { x: 1, y: 2, c: [0] },
+            { x: 2, y: 2, c: [0] },
+            { x: 0, y: 3, c: [0] },
+            { x: 1, y: 3, c: [0] },
+            { x: 2, y: 3, c: [0] },
+            { x: 0, y: 4, c: [0] },
+            { x: 2, y: 4, c: [0] },
+        ]
+    );
+
+    const shortboy = new Variant(
+        Amongy.Type.SHORT,
+        false,
+        [
+            { x: 0, y: 0, c: [0] },
+            { x: 1, y: 0, c: [0] },
+            { x: 2, y: 0, c: [0] },
+            { x: -1, y: 1, c: [0] },
+            { x: 0, y: 1, c: [0] },
+            { x: 1, y: 1, c: [3] },
+            { x: 2, y: 1, c: [3] },
+            { x: -1, y: 2, c: [0] },
+            { x: 0, y: 2, c: [0] },
+            { x: 1, y: 2, c: [0] },
+            { x: 2, y: 2, c: [0] },
+            { x: 0, y: 3, c: [0] },
+            { x: 2, y: 3, c: [0] },
+        ]
+    );
+
+    //Order is very important here, check for bigger variants first because it will (almost) always find the smaller variant in the bigger variant.
+    const variants = [traditionalboy, flip(traditionalboy), shortboy, flip(shortboy)];
+
+
+    let canvasWidth;
+    let canvasHeight;
+    const amongyCollection = [];
+    const pixelMatrix = [[]];
+
+
+    const checkImageForAmongy = (data) => {
+        for (let y = 0; y < canvasWidth; y++)
+            for (let x = 0; x < canvasHeight; x++) {
+                if (pixelMatrix[x] && pixelMatrix[x][y]) continue;
+
+                for (let i = 0; i < variants.length; i++) {
+                    pixels = checkVariant(data, variants[i], x, y);
+                    if (!pixels) continue;
+                    addVariantToMatrix(pixels);
+                    amongyCollection.push(new Amongy(variants[i].type, pixels));
+                    break;
+                }
+            }
+
+        postMessage([amongyCollection, pixelMatrix])
+    }
+
+    const checkVariant = (data, variant, startX, startY) => {
+        if (!checkCords(startX, startY)) return false;
+        let colors = [];
+        let response = [];
+
+        for (let i = 0; i < variant.cords.length; i++) {
+            let cord = variant.cords[i];
+            let x = startX + cord.x;
+            let y = startY + cord.y;
+            if (!checkCords(x, y)) return false;
+
+            let color = getRGBFromCords(data, x, y);
+
+            correct = false;
+            for (let cv = 0; cv < cord.c.length; cv++) {
+                if (!colors[cord.c[cv]] && !colors.includes(color))
+                    colors[cord.c[cv]] = color;
+
+                if (colors[cord.c[cv]] == color) {
+                    correct = true;
+                    break;
+                }
+            }
+            if (correct) response.push(new Pixel(x, y, color));
+            else return false;
+        }
+        return response;
+    }
+
+    const addVariantToMatrix = (pixels) => {
+        for (let i = 0; i < pixels.length; i++) {
+            if (!pixelMatrix[pixels[i].x])
+                pixelMatrix[pixels[i].x] = [];
+            pixelMatrix[pixels[i].x][pixels[i].y] = pixels[i].color;
+        }
+    }
+
+    const checkCords = (x, y) => {
+        return !(x < 0 || x >= canvasWidth || y < 0 || y >= canvasHeight)
+    }
+
+    const getRGBFromCords = (data, x, y) => {
+        const r = data[canvasWidth * y * 4 + x * 4 + 0];
+        const g = data[canvasWidth * y * 4 + x * 4 + 1];
+        const b = data[canvasWidth * y * 4 + x * 4 + 2];
+        return `${r},${g},${b}`;
+    }
+}
--- scripts/analyser.js
+++ scripts/analyser.js
@@ -1,106 +1,9 @@
-class Amongy {
-    static Type = {
-        SHORT: "short",
-        TRADITIONAL: "traditional",
-        SITTING: "sitting",
-        NONE: "none"
-    }
-
-    type = Amongy.Type.NONE;
-    pixels = [];
-
-    constructor(type, pixels) {
-        this.type = type;
-        this.pixels = pixels;
-    }
-}
-
-class Pixel {
-    x = 0;
-    y = 0;
-    color = "" //RGB
-
-    constructor(x, y, color) {
-        this.x = x;
-        this.y = y;
-        this.color = color;
-    }
-}
-
-const flip = (variant) => {
-    let cords = [];
-    oldX = Math.max(...variant.cords.filter(v => v.y == 0).map(v => v.x));
-    variant.cords.forEach(cord => {
-        cords.push({ x: Math.abs(cord.x - oldX), y: cord.y, c: cord.c });
-    });
-
-    return new Variant(variant.type, true, cords);
-}
-
-class Variant {
-    type = Amongy.Type.NONE;
-    flipped = false;
-    cords = [];
-
-    constructor(type, flipped, cords) {
-        this.type = type;
-        this.flipped = flipped;
-        this.cords = cords;
-    }
-}
-
-const traditionalboy = new Variant(
-    Amongy.Type.TRADITIONAL,
-    false,
-    [
-        { x: 0, y: 0, c: [0] },
-        { x: 1, y: 0, c: [0] },
-        { x: 2, y: 0, c: [0] },
-        { x: -1, y: 1, c: [0] },
-        { x: 0, y: 1, c: [0] },
-        { x: 1, y: 1, c: [3] },
-        { x: 2, y: 1, c: [3] },
-        { x: -1, y: 2, c: [0] },
-        { x: 0, y: 2, c: [0] },
-        { x: 1, y: 2, c: [0] },
-        { x: 2, y: 2, c: [0] },
-        { x: 0, y: 3, c: [0] },
-        { x: 1, y: 3, c: [0] },
-        { x: 2, y: 3, c: [0] },
-        { x: 0, y: 4, c: [0] },
-        { x: 2, y: 4, c: [0] },
-    ]
-);
-
-const shortboy = new Variant(
-    Amongy.Type.SHORT,
-    false,
-    [
-        { x: 0, y: 0, c: [0] },
-        { x: 1, y: 0, c: [0] },
-        { x: 2, y: 0, c: [0] },
-        { x: -1, y: 1, c: [0] },
-        { x: 0, y: 1, c: [0] },
-        { x: 1, y: 1, c: [3] },
-        { x: 2, y: 1, c: [3] },
-        { x: -1, y: 2, c: [0] },
-        { x: 0, y: 2, c: [0] },
-        { x: 1, y: 2, c: [0] },
-        { x: 2, y: 2, c: [0] },
-        { x: 0, y: 3, c: [0] },
-        { x: 2, y: 3, c: [0] },
-    ]
-);
-
-//Order is very important here, check for bigger variants first because it will (almost) always find the smaller variant in the bigger variant.
-const variants = [traditionalboy, flip(traditionalboy), shortboy, flip(shortboy)];
-
 const analyseButton = document.getElementById("btn-analyse");
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext('2d');
 
-const amongyCollection = [];
-const pixelMatrix = [[]]
+let amongyCollection;
+let pixelMatrix;
 
 const place = new Image();
 place.src = base64Image;
@@ -119,76 +22,19 @@ const loadIcon = () => {
 
 const checkImageForAmongy = () => {
     analyseButton.innerHTML = loadIcon().outerHTML;
+    let data = ctx.getImageData(0, 0, canvas.width, canvas.height).data;
 
-    //set timeout to let the load icon appear, this may be fixed when switched to a worker.
-    setTimeout(() => {
-        let data = ctx.getImageData(0, 0, canvas.width, canvas.height).data;
-        for (let y = 0; y < canvas.width; y++)
-            for (let x = 0; x < canvas.height; x++) {
-                if (pixelMatrix[x] && pixelMatrix[x][y]) continue;
+    let worker = new Worker(URL.createObjectURL(new Blob(["("+worker_function.toString()+")()"], {type: 'text/javascript'})));
+    worker.postMessage([data, canvas.width, canvas.height]);
 
-                for (let i = 0; i < variants.length; i++) {
-                    pixels = checkVariant(data, variants[i], x, y);
-                    if (!pixels) continue;
-                    addVariantToMatrix(pixels);
-                    amongyCollection.push(new Amongy(variants[i].type, pixels));
-                    break;
-                }
-            }
-
-        initialiseCanvas()
-        addDarkOverlay(.8);
-        showAmongy();
-        analyseButton.innerHTML = "Analyse";
-    }, 10);
-}
-
-const checkVariant = (data, variant, startX, startY) => {
-    if (!checkCords(startX, startY)) return false;
-    let colors = [];
-    let response = [];
-
-    for (let i = 0; i < variant.cords.length; i++) {
-        let cord = variant.cords[i];
-        let x = startX + cord.x;
-        let y = startY + cord.y;
-        if (!checkCords(x, y)) return false;
-
-        let color = getRGBFromCords(data, x, y);
-
-        correct = false;
-        for (let cv = 0; cv < cord.c.length; cv++) {
-            if (!colors[cord.c[cv]] && !colors.includes(color))
-                colors[cord.c[cv]] = color;
-
-            if (colors[cord.c[cv]] == color) {
-                correct = true;
-                break;
-            }
-        }
-        if (correct) response.push(new Pixel(x, y, color));
-        else return false;
+    worker.onmessage = (message) => {
+        console.log(message);
+        // initialiseCanvas()
+        // addDarkOverlay(.8);
+        // showAmongy();
+        // analyseButton.innerHTML = "Analyse";
+        // worker.terminate();
     }
-    return response;
-}
-
-const addVariantToMatrix = (pixels) => {
-    for (let i = 0; i < pixels.length; i++) {
-        if (!pixelMatrix[pixels[i].x])
-            pixelMatrix[pixels[i].x] = [];
-        pixelMatrix[pixels[i].x][pixels[i].y] = pixels[i].color;
-    }
-}
-
-const checkCords = (x, y) => {
-    return !(x < 0 || x >= canvas.width || y < 0 || y >= canvas.height)
-}
-
-const getRGBFromCords = (data, x, y) => {
-    const r = data[canvas.width * y * 4 + x * 4 + 0];
-    const g = data[canvas.width * y * 4 + x * 4 + 1];
-    const b = data[canvas.width * y * 4 + x * 4 + 2];
-    return `${r},${g},${b}`;
 }
 
 const initialiseCanvas = () => {
